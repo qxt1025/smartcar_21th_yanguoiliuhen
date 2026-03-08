67708fde4b3c2ac0a832f5f8873a01f01dd68f91
qxt1025 <xycpbhwm9q@privaterelay.appleid.com>
2026-03-08 11:43:38 +0800

    feat: motor

--- basic_project_0211_zebra/Base/motor.h
+++ basic_project_0211_zebra/Base/motor.h
@@ -1,6 +1,12 @@
 #ifndef CODE_BASE_MOTOR_H_
 #define CODE_BASE_MOTOR_H_
 #include "base.h"
+
+#define RIGHT_MOTOR_PWM_PIN PWMB_CH1_P74
+#define RIGHT_MOTOR_DIR_PIN IO_P75
+#define LEFT_MOTOR_PWM_PIN  PWMB_CH3_P76
+#define LEFT_MOTOR_DIR_PIN  IO_P77
+
 #define STEER_MID 4850
 void motor_init();
 void set_pwm(int PWM_L,int PWM_R);
